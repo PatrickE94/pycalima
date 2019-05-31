23e7bca43270bb3fe1e47fff7d383d90a3cbbda0
Patrick Engström <patrick@weblab.se>
2019-05-31 16:03:48 +0200

    Moved to use Characteristics UUID
    Decompiled the Android app to retrieve all characteristics and their
    intentions. All have not been implemented.

--- pycalima/Calima.py
+++ pycalima/Calima.py
@@ -29,13 +29,40 @@ Fanspeeds.__new__.__defaults__ = (2250, 1625, 1000)
 Time = namedtuple('Time', 'DayOfWeek Hour Minute Second')
 Sensitivity = namedtuple('Sensitivity', 'HumidityOn Humidity LightOn Light')
 LightSensorSettings = namedtuple('LightSensorSettings', 'DelayedStart RunningTime')
+HeatDistributorSettings = namedtuple('HeatDistributorSettings', 'TemperatureLimit FanSpeedBelow FanSpeedAbove')
 SilentHours = namedtuple('SilentHours', 'On StartingHour StartingMinute EndingHour EndingMinute')
 TrickleDays = namedtuple('TrickleDays', 'Weekdays Weekends')
 BoostMode = namedtuple('BoostMode', 'OnOff Speed Seconds')
 
-FanState = namedtuple('FanState', 'Humidity Temp Light RPM BoostActive Mode Unknown Unknown2')
-FanStateShort = namedtuple('FanState', 'Humidity Temp Light RPM BoostActive Mode')
+FanState = namedtuple('FanState', 'Humidity Temp Light RPM Trigger Tbd')
 
+# Stolen defines for each characteristic (taken from a decompiled Android App)
+CHARACTERISTIC_APPEARANCE = "00002a01-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_AUTOMATIC_CYCLES = "f508408a-508b-41c6-aa57-61d1fd0d5c39"
+CHARACTERISTIC_BASIC_VENTILATION = "faa49e09-a79c-4725-b197-bdc57c67dc32"
+CHARACTERISTIC_BOOST = "118c949c-28c8-4139-b0b3-36657fd055a9"
+CHARACTERISTIC_CLOCK = "6dec478e-ae0b-4186-9d82-13dda03c0682"
+CHARACTERISTIC_DEVICE_NAME = "00002a00-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_FACTORY_SETTINGS_CHANGED = "63b04af9-24c0-4e5d-a69c-94eb9c5707b4"
+CHARACTERISTIC_FAN_DESCRIPTION = "b85fa07a-9382-4838-871c-81d045dcc2ff"
+CHARACTERISTIC_FIRMWARE_REVISION = "00002a26-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_HARDWARE_REVISION = "00002a27-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_LED = "8b850c04-dc18-44d2-9501-7662d65ba36e"
+CHARACTERISTIC_LEVEL_OF_FAN_SPEED = "1488a757-35bc-4ec8-9a6b-9ecf1502778e"
+CHARACTERISTIC_MANUFACTURER_NAME = "00002a29-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_MODE = "90cabcd1-bcda-4167-85d8-16dcd8ab6a6b"
+CHARACTERISTIC_MODEL_NUMBER = "00002a24-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_NIGHT_MODE = "b5836b55-57bd-433e-8480-46e4993c5ac0"
+CHARACTERISTIC_PIN_CODE = "4cad343a-209a-40b7-b911-4d9b3df569b2"
+CHARACTERISTIC_PIN_CONFIRMATION = "d1ae6b70-ee12-4f6d-b166-d2063dcaffe1"
+CHARACTERISTIC_RESET = "ff5f7c4f-2606-4c69-b360-15aaea58ad5f"
+CHARACTERISTIC_SENSITIVITY = "e782e131-6ce1-4191-a8db-f4304d7610f1"
+CHARACTERISTIC_SENSOR_DATA = "528b80e8-c47a-4c0a-bdf1-916a7748f412"
+CHARACTERISTIC_SERIAL_NUMBER = "00002a25-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_SOFTWARE_REVISION = "00002a28-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_STATUS = "25a824ad-3021-4de9-9f2f-60cf8d17bded"
+CHARACTERISTIC_TEMP_HEAT_DISTRIBUTOR = "a22eae12-dba8-49f3-9c69-1721dcff1d96"
+CHARACTERISTIC_TIME_FUNCTIONS = "49c616de-02b1-4b67-b237-90f66793a6f2"
 
 def FindCalimas():
     scanner = ble.Scanner()
@@ -111,40 +138,53 @@ class Calima:
     def getManufacturer(self):
         return self._readHandle(0x15).decode('ascii')
 
-    # --- Onwards to PAX "unknown" characteristics
+    # --- Onwards to PAX characteristics
 
     def setAuth(self, pin):
-        self._writeUUID("4cad343a-209a-40b7-b911-4d9b3df569b2", pack("<I", int(pin)))
+        self._writeUUID(CHARACTERISTIC_PIN_CODE, pack("<I", int(pin)))
+
+    def checkAuth(self):
+        return bool(unpack('<I', self._readUUID(CHARACTERISTIC_PIN_CONFIRMATION)))
 
     def setAlias(self, name):
-        self._writeUUID("b85fa07a-9382-4838-871c-81d045dcc2ff", pack('20s', bytearray(name, 'utf-8')))
+        self._writeUUID(CHARACTERISTIC_FAN_DESCRIPTION, pack('20s', bytearray(name, 'utf-8')))
 
     def getAlias(self):
-        return self._readUUID("b85fa07a-9382-4838-871c-81d045dcc2ff").decode('utf-8')
+        return self._readUUID(CHARACTERISTIC_FAN_DESCRIPTION).decode('utf-8')
 
     def getIsClockSet(self):
-        return self._bToStr(self._readUUID("25a824ad-3021-4de9-9f2f-60cf8d17bded"))
-
-    def getStateShort(self):
-        v = unpack('<4HBHB', self._readUUID("528b80e8-c47a-4c0a-bdf1-916a7748f412"))
-        boostMode = bool(v[4] & 0x10)
-        mode = v[4] >> 1
-        #return self._bToStr(self._readHandleShort(0x21))
-        # Among other things round temperature to 1 decimal point
-        return FanStateShort(v[0], round((v[1]/4),1), v[2], v[3], boostMode, mode)
+        return self._bToStr(self._readUUID(CHARACTERISTIC_STATUS))
 
     def getState(self):
-        v = unpack('<4HBHB', self._readUUID("528b80e8-c47a-4c0a-bdf1-916a7748f412"))
-        boostMode = bool(v[4] & 0x10)
-        #mode = ('Unknown', 'Trickle', 'Light', 'Humidity')[v[4] & ~(0x10)]
-        mode = v[4] >> 1
-        return FanState(v[0], v[1]/4, v[2], v[3], boostMode, mode, v[5], v[6])
+        # Short Short Short Short    byte           byte
+        # Hum   Temp  Light FanSpeed CurrentTrigger Tbd
+        v = unpack('<4HBB', self._readUUID(CHARACTERISTIC_SENSOR_DATA))
+        trigger = "No trigger"
+        if ((v[4] >> 4) & 1) == 1:
+            trigger = "Boost"
+        elif (v[4] & 3) == 1:
+            trigger = "Trickle ventilation"
+        elif (v[4] & 3) == 2:
+            trigger = "Light ventilation"
+        elif (v[4] & 3) == 3: # Note that the trigger might be active, but mode must be enabled to be activated
+            trigger = "Humidity ventilation"
+        return FanState(v[0], v[1]/4, v[2], v[3], trigger, v[5])
 
-    def getUnknown24(self):
-        return self._bToStr(self._readUUID("63b04af9-24c0-4e5d-a69c-94eb9c5707b4"))
+    def getFactorySettingsChanged(self):
+        return bool(unpack('<I', self._readUUID(CHARACTERISTIC_FACTORY_SETTINGS_CHANGED)))
 
-    def getUnknown26(self):
-        return self._bToStr(self._readUUID("90cabcd1-bcda-4167-85d8-16dcd8ab6a6b"))
+    def getMode(self):
+        v = unpack('<H', self._readUUID(CHARACTERISTIC_MODE))
+        if v == 0:
+            return "MultiMode"
+        elif v == 1:
+            return "DraftShutterMode"
+        elif v == 2:
+            return "WallSwitchExtendedRuntimeMode"
+        elif v == 3:
+            return "WallSwitchNoExtendedRuntimeMode"
+        elif v == 4:
+            return "HeatDistributionMode"
 
     def setFanSpeedSettings(self, humidity=2250, light=1625, trickle=1000):
         for val in (humidity, light, trickle):
@@ -153,10 +193,10 @@ class Calima:
             if (val > 2500 or val < 0):
                 raise ValueError("Speeds must be between 0 and 2500 rpm")
 
-        self._writeUUID("1488a757-35bc-4ec8-9a6b-9ecf1502778e", pack('<HHH', humidity, light, trickle))
+        self._writeUUID(CHARACTERISTIC_LEVEL_OF_FAN_SPEED, pack('<HHH', humidity, light, trickle))
 
     def getFanSpeedSettings(self):
-        return Fanspeeds._make(unpack('<HHH', self._readUUID("1488a757-35bc-4ec8-9a6b-9ecf1502778e")))
+        return Fanspeeds._make(unpack('<HHH', self._readUUID(CHARACTERISTIC_LEVEL_OF_FAN_SPEED)))
 
     def setSensorsSensitivity(self, humidity, light):
         if humidity > 3 or humidity < 0:
@@ -165,10 +205,11 @@ class Calima:
             raise ValueError("Light sensitivity must be between 0-3")
 
         value = pack('<4B', bool(humidity), humidity, bool(light), light)
-        self._writeUUID("e782e131-6ce1-4191-a8db-f4304d7610f1", value)
+        self._writeUUID(CHARACTERISTIC_SENSITIVITY, value)
 
     def getSensorsSensitivity(self):
-        return Sensitivity._make(unpack('<4B', self._readUUID("e782e131-6ce1-4191-a8db-f4304d7610f1")))
+        # Hum Active | Hum Sensitivity | Light Active | Light Sensitivity
+        return Sensitivity._make(unpack('<4B', self._readUUID(CHARACTERISTIC_SENSITIVITY)))
 
     def setLightSensorSettings(self, delayed, running):
         if delayed not in (0, 5, 10):
@@ -176,13 +217,13 @@ class Calima:
         if running not in (5, 10, 15, 30, 60):
             raise ValueError("Running time must be 5, 10, 15, 30 or 60 minutes")
 
-        self._writeUUID("49c616de-02b1-4b67-b237-90f66793a6f2", pack('<2B', delayed, running))
+        self._writeUUID(CHARACTERISTIC_TIME_FUNCTIONS, pack('<2B', delayed, running))
 
     def getLightSensorSettings(self):
-        return LightSensorSettings._make(unpack('<2B', self._readUUID("49c616de-02b1-4b67-b237-90f66793a6f2")))
+        return LightSensorSettings._make(unpack('<2B', self._readUUID(CHARACTERISTIC_TIME_FUNCTIONS)))
 
-    def getUnknown2e(self):
-        return self._bToStr(self._readUUID("a22eae12-dba8-49f3-9c69-1721dcff1d96"))
+    def getHeatDistributor(self):
+        return HeatDistributorSettings._make(unpack('<BHH', self._readUUID(CHARACTERISTIC_TEMP_HEAT_DISTRIBUTOR)))
 
     def setBoostMode(self, on, speed, seconds):
         if speed % 25:
@@ -191,28 +232,28 @@ class Calima:
             speed = 0
             seconds = 0
 
-        self._writeUUID("118c949c-28c8-4139-b0b3-36657fd055a9", pack('<BHH', on, speed, seconds))
+        self._writeUUID(CHARACTERISTIC_BOOST, pack('<BHH', on, speed, seconds))
 
     def getBoostMode(self):
-        return BoostMode._make(unpack('<BHH', self._readUUID("118c949c-28c8-4139-b0b3-36657fd055a9")))
+        return BoostMode._make(unpack('<BHH', self._readUUID(CHARACTERISTIC_BOOST)))
 
-    def getUnknown32(self):
-        return self._bToStr(self._readUUID("8b850c04-dc18-44d2-9501-7662d65ba36e"))
+    def getLed(self):
+        return self._bToStr(self._readUUID(CHARACTERISTIC_LED))
 
     def setAutomaticCycles(self, setting):
         if setting < 0 or setting > 3:
             raise ValueError("Setting must be between 0-3")
 
-        self._writeUUID("f508408a-508b-41c6-aa57-61d1fd0d5c39", pack('<B', setting))
+        self._writeUUID(CHARACTERISTIC_AUTOMATIC_CYCLES, pack('<B', setting))
 
     def getAutomaticCycles(self):
-        return unpack('<B', self._readUUID("f508408a-508b-41c6-aa57-61d1fd0d5c39"))[0]
+        return unpack('<B', self._readUUID(CHARACTERISTIC_AUTOMATIC_CYCLES))[0]
 
     def setTime(self, dayofweek, hour, minute, second):
-        self._writeUUID("6dec478e-ae0b-4186-9d82-13dda03c0682", pack('<4B', dayofweek, hour, minute, second))
+        self._writeUUID(CHARACTERISTIC_CLOCK, pack('<4B', dayofweek, hour, minute, second))
 
     def getTime(self):
-        return Time._make(unpack('<BBBB', self._readUUID("6dec478e-ae0b-4186-9d82-13dda03c0682")))
+        return Time._make(unpack('<BBBB', self._readUUID(CHARACTERISTIC_CLOCK)))
 
     def setTimeToNow(self):
         now = datetime.datetime.now()
@@ -231,16 +272,22 @@ class Calima:
         value = pack('<5B', int(on),
                      startingHours, startingMinutes,
                      endingHours, endingMinutes)
-        self._writeUUID("b5836b55-57bd-433e-8480-46e4993c5ac0", value)
+        self._writeUUID(CHARACTERISTIC_NIGHT_MODE, value)
 
     def getSilentHours(self):
-        return SilentHours._make(unpack('<5B', self._readUUID("b5836b55-57bd-433e-8480-46e4993c5ac0")))
+        return SilentHours._make(unpack('<5B', self._readUUID(CHARACTERISTIC_NIGHT_MODE)))
 
     def setTrickleDays(self, weekdays, weekends):
-        self._writeUUID("faa49e09-a79c-4725-b197-bdc57c67dc32", pack('<2B', weekdays, weekends))
+        self._writeUUID(CHARACTERISTIC_BASIC_VENTILATION, pack('<2B', weekdays, weekends))
 
     def getTrickleDays(self):
-        return TrickleDays._make(unpack('<2B', self._readUUID("faa49e09-a79c-4725-b197-bdc57c67dc32")))
+        return TrickleDays._make(unpack('<2B', self._readUUID(CHARACTERISTIC_BASIC_VENTILATION)))
 
-    def getUnknown3c(self):
-        return self._readUUID("ff5f7c4f-2606-4c69-b360-15aaea58ad5f")
+    def getReset(self): # Should be write
+        return self._readUUID(CHARACTERISTIC_RESET)
+
+    def resetDevice(self): # Dangerous
+        self._writeUUID(CHARACTERISTIC_RESET, pack('<I', 120))
+
+    def resetValues(self): # Danguerous
+        self._writeUUID(CHARACTERISTIC_RESET, pack('<I', 85))
